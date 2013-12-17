8d53a19e6c05941c09f78e948206e6f0d5c7bdae
Jianyang <athena2651cn@Jianyang-LImatoMacBook-Pro.local>
2013-12-17 22:08:42 +0100

    deleted 未命名文件夹

--- 未命名文件夹/includes/db.php
+++
@@ -1,5 +0,0 @@
-<?
-	@mysql_connect("localhost","root","root") or die("Problème de connection à la Base de données");
-	@mysql_select_db("shopping") or die("La base de données n'est pas accèssible");
-	session_start();
-?>
